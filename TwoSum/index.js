2abf4716c320ee4f2253b83a94e943d54815dbe9
danixtra <danishasim403@gmail.com>
2026-02-15 01:29:48 -0500

    Two Sum -> Solved

---
+++ TwoSum/index.js
@@ -0,0 +1,37 @@
+// // Brute Force
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//     const res =[]
+//     for (let i=0; i<nums.length; i++) {
+//         for (let j = 0; j<nums.length; j++) {
+//             if (i!=j && nums[i] + nums[j] == target) {
+//                 res.push(...[i,j])
+//                 return res;
+//             }
+//         }
+//     }
+// };
+
+
+/** 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const res = new Map()
+    const final = []
+    for (let i = 0; i<nums.length; i++) {
+        const value = target - nums[i]
+        if(res.has(value)) {
+            final.push(res.get(target - nums[i]), i)
+            break
+        }
+        res.set(nums[i], i);
+    }
+    return final
+};
